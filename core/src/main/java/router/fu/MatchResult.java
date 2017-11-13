6d6b793b7adba9de7cd5d6d8a2116f7edcafbeff
Peter Donald <peter@realityforge.org>
2017-11-13 16:38:26 +1100

    Add some javadocs

--- core/src/main/java/router/fu/MatchResult.java
+++ core/src/main/java/router/fu/MatchResult.java
@@ -1,8 +1,20 @@
 package router.fu;
 
+/**
+ * Enum that determines how to proceed after a basic path match.
+ */
 public enum MatchResult
 {
+  /**
+   * No match. Skip route and continue matching.
+   */
   NO_MATCH,
+  /**
+   * Route is matched but continue to collect other matching routes.
+   */
   NON_TERMINAL,
+  /**
+   * Route is matched and stop looking for other routes to match.
+   */
   TERMINAL
 }
