6e10a14e5739a5d0ecbb6f84a35be60d23218a02
Peter Donald <peter@realityforge.org>
2017-11-14 15:44:31 +1100

    Add RoutingBackend and implementation to perform routing

---
+++ core/src/main/java/router/fu/Elemental2HashRoutingBackend.java
@@ -0,0 +1,79 @@
+package router.fu;
+
+import elemental2.dom.DomGlobal;
+import elemental2.dom.EventListener;
+import elemental2.dom.Window;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import jsinterop.base.JsPropertyMap;
+
+/**
+ * Hash based routing backend based on Elemental2.
+ */
+public class Elemental2HashRoutingBackend
+  implements RoutingBackend
+{
+  @Nonnull
+  private final Window _window;
+
+  /**
+   * Create backend for routing for on current window.
+   */
+  public Elemental2HashRoutingBackend()
+  {
+    this( DomGlobal.window );
+  }
+
+  /**
+   * Create backend for routing for specified window.
+   *
+   * @param window the window.
+   */
+  public Elemental2HashRoutingBackend( @Nonnull final Window window )
+  {
+    _window = Objects.requireNonNull( window );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nullable
+  @Override
+  public String getLocation()
+  {
+    final String hash = (String) JsPropertyMap.of( _window.location ).get( "hash" );
+    return null == hash ? null : hash.substring( 1 );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setLocation( @Nullable final String hash )
+  {
+    final String value = null == hash ? null : "#" + hash;
+    JsPropertyMap.of( _window.location ).set( "hash", value );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  @Override
+  public Object addListener( @Nonnull final LocationChangeListener handler )
+  {
+    final EventListener eventListener = e -> handler.onLocationChange( getLocation() );
+    _window.addEventListener( "hashchange", eventListener, false );
+    return eventListener;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void removeListener( @Nonnull final Object handle )
+  {
+    _window.removeEventListener( "hashchange", (EventListener) handle, false );
+  }
+}
---
+++ core/src/main/java/router/fu/RoutingBackend.java
@@ -0,0 +1,47 @@
+package router.fu;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Interface via which different routing backends can be implemented.
+ */
+public interface RoutingBackend
+{
+  @FunctionalInterface
+  interface LocationChangeListener
+  {
+    boolean onLocationChange( @Nullable String location );
+  }
+
+  /**
+   * Return the location.
+   *
+   * @return the location.
+   */
+  @Nullable
+  String getLocation();
+
+  /**
+   * Explicitly set the location.
+   *
+   * @param location the location.
+   */
+  void setLocation( @Nullable String location );
+
+  /**
+   * Register a listener that is called back when location changes.
+   *
+   * @param handler the callback handler.
+   * @return the handle that can be passed to {@link #removeListener(Object)} to remove listener.
+   */
+  @Nonnull
+  Object addListener( @Nonnull LocationChangeListener handler );
+
+  /**
+   * De-register listener so it will not receive notifications when location changes.
+   *
+   * @param handle the handle returned by the {@link #addListener(LocationChangeListener)} method.
+   */
+  void removeListener( @Nonnull Object handle );
+}
