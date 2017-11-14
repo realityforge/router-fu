39ac1ab1f879083534dee3c2922b50becf73a5fd
Peter Donald <peter@realityforge.org>
2017-11-14 14:40:52 +1100

    Add RouteLocation object

---
+++ core/src/main/java/router/fu/RouteLocation.java
@@ -0,0 +1,55 @@
+package router.fu;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Result of performing a routing on location.
+ * The result is an ordered list of {@link RouteState} instances with the last instance
+ * potentially being a terminal routing state and other states being non-terminal.
+ */
+public final class RouteLocation
+{
+  @Nonnull
+  private final List<RouteState> _states;
+
+  public RouteLocation( @Nonnull final List<RouteState> states )
+  {
+    _states = Collections.unmodifiableList( Objects.requireNonNull( states ) );
+  }
+
+  /**
+   * Return the terminal state of routing if any.
+   * This is essentially the state that the Route identified as terminal.
+   * It will be the last one in the list if it exists.
+   *
+   * @return the terminal state or null if no such state.
+   */
+  @Nullable
+  public RouteState getTerminalState()
+  {
+    if ( _states.isEmpty() )
+    {
+      return null;
+    }
+    else
+    {
+      final RouteState state = _states.get( _states.size() - 1 );
+      return state.isTerminal() ? state : null;
+    }
+  }
+
+  /**
+   * Return the list of route states produced during routing.
+   *
+   * @return the list of route states produced during routing.
+   */
+  @Nonnull
+  public List<RouteState> getStates()
+  {
+    return _states;
+  }
+}
---
+++ core/src/test/java/router/fu/RouteLocationTest.java
@@ -0,0 +1,66 @@
+package router.fu;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import javax.annotation.Nonnull;
+import org.realityforge.guiceyloops.shared.ValueUtil;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class RouteLocationTest
+{
+  @Test
+  public void basicOperation()
+  {
+    final RouteState state1 = newRouteState( false );
+    final RouteState state2 = newRouteState( false );
+    final RouteState state3 = newRouteState( false );
+    final RouteState state4 = newRouteState( true );
+
+    final RouteLocation location = new RouteLocation( Arrays.asList( state1, state2, state3, state4 ) );
+    assertEquals( location.getStates().size(), 4 );
+    assertEquals( location.getStates().get( 0 ), state1 );
+    assertEquals( location.getStates().get( 1 ), state2 );
+    assertEquals( location.getStates().get( 2 ), state3 );
+    assertEquals( location.getStates().get( 3 ), state4 );
+    assertEquals( location.getTerminalState(), state4 );
+  }
+
+  @Test
+  public void noTerminal()
+  {
+    final RouteState state1 = newRouteState( false );
+    final RouteState state2 = newRouteState( false );
+
+    final RouteLocation location = new RouteLocation( Arrays.asList( state1, state2 ) );
+    assertEquals( location.getStates().size(), 2 );
+    assertEquals( location.getStates().get( 0 ), state1 );
+    assertEquals( location.getStates().get( 1 ), state2 );
+    assertEquals( location.getTerminalState(), null );
+  }
+
+  @Test
+  public void noState()
+  {
+    final RouteLocation location = new RouteLocation( Collections.emptyList() );
+    assertEquals( location.getStates().size(), 0 );
+    assertEquals( location.getTerminalState(), null );
+  }
+
+  @Nonnull
+  private RouteState newRouteState( final boolean terminal )
+  {
+    return new RouteState( newRoute(), new HashMap<>(), terminal );
+  }
+
+  @Nonnull
+  private Route newRoute()
+  {
+    return new Route( ValueUtil.randomString(),
+                      new PathElement[ 0 ],
+                      new PathParameter[ 0 ],
+                      new TestRegExp(),
+                      new TestRouteMatchCallback() );
+  }
+}
