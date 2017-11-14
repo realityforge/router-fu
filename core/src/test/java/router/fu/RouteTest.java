5bc05cfa5f28730ae1e3213bdcca448b00bfc7b4
Peter Donald <peter@realityforge.org>
2017-11-14 11:48:02 +1100

    Add some tests for match

--- core/src/test/java/router/fu/RouteTest.java
+++ core/src/test/java/router/fu/RouteTest.java
@@ -26,6 +26,146 @@ public class RouteTest
   }
 
   @Test
+  public void match_noParameters()
+  {
+    final RouteMatchCallback matchCallback = new TestRouteMatchCallback();
+    final String name = ValueUtil.randomString();
+    final String location = ValueUtil.randomString();
+    final TestRegExp matcher = new TestRegExp( new String[]{ location } );
+    final Route route = new Route( name, null, new PathParameter[ 0 ], matcher, matchCallback );
+
+    final RouteState state = route.match( location );
+    assertNotNull( state );
+
+    assertEquals( state.getRoute(), route );
+    assertEquals( state.getParameters().isEmpty(), true );
+    assertEquals( state.isTerminal(), true );
+  }
+
+  @Test
+  public void match_nonTermninal()
+  {
+    final RouteMatchCallback matchCallback = new TestRouteMatchCallback( MatchResult.NON_TERMINAL );
+    final String name = ValueUtil.randomString();
+    final String location = ValueUtil.randomString();
+    final TestRegExp matcher = new TestRegExp( new String[]{ location } );
+    final Route route = new Route( name, null, new PathParameter[ 0 ], matcher, matchCallback );
+
+    final RouteState state = route.match( location );
+    assertNotNull( state );
+
+    assertEquals( state.getRoute(), route );
+    assertEquals( state.getParameters().isEmpty(), true );
+    assertEquals( state.isTerminal(), false );
+  }
+
+  @Test
+  public void match_matchButCallbackMakesNonMatch()
+  {
+    final RouteMatchCallback matchCallback = new TestRouteMatchCallback( MatchResult.NO_MATCH );
+    final String name = ValueUtil.randomString();
+    final String location = ValueUtil.randomString();
+    final TestRegExp matcher = new TestRegExp( new String[]{ location } );
+    final Route route = new Route( name, null, new PathParameter[ 0 ], matcher, matchCallback );
+
+    final RouteState state = route.match( location );
+    assertNull( state );
+  }
+
+  @Test
+  public void match_noMatch()
+  {
+    final RouteMatchCallback matchCallback = new TestRouteMatchCallback();
+    final String name = ValueUtil.randomString();
+    final PathParameter[] parameters = {
+      new PathParameter( "paramA" ),
+      new PathParameter( "paramB" )
+    };
+    final String location = "/locations/ballarat/events/42";
+    final TestRegExp matcher = new TestRegExp();
+    final Route route =
+      new Route( name, null, parameters, matcher, matchCallback );
+
+    final RouteState state = route.match( location );
+    assertNull( state );
+  }
+
+  @Test
+  public void match_simpleParameters()
+  {
+    final RouteMatchCallback matchCallback = new TestRouteMatchCallback();
+    final String name = ValueUtil.randomString();
+    final PathParameter[] parameters = {
+      new PathParameter( "paramA" ),
+      new PathParameter( "paramB" )
+    };
+    final String location = "/locations/ballarat/events/42";
+    final String[] resultGroups = { location, "ballarat", "42" };
+
+    //Assume a regexp like "^/locations/([^/]+)/events/(\d+)$"
+    final TestRegExp matcher = new TestRegExp( resultGroups );
+    final Route route =
+      new Route( name, null, parameters, matcher, matchCallback );
+
+    final RouteState state = route.match( location );
+    assertNotNull( state );
+
+    assertEquals( state.getRoute(), route );
+    assertEquals( state.getParameters().size(), 2 );
+    assertEquals( state.getParameters().get( "paramA" ), "ballarat" );
+    assertEquals( state.getParameters().get( "paramB" ), "42" );
+    assertEquals( state.isTerminal(), true );
+  }
+
+  @Test
+  public void match_complexParameters()
+  {
+    final RouteMatchCallback matchCallback = new TestRouteMatchCallback();
+    final String name = ValueUtil.randomString();
+    final PathParameter[] parameters = {
+      new PathParameter( "paramA", new TestRegExp( new String[]{ "ballarat" } ) ),
+      new PathParameter( "paramB", new TestRegExp( new String[]{ "42" } ) )
+    };
+    final String location = "/locations/ballarat/events/42";
+    final String[] resultGroups = { location, "ballarat", "42" };
+
+    //Assume a regexp like "^/locations/([^/]+)/events/(\d+)$"
+    final TestRegExp matcher = new TestRegExp( resultGroups );
+    final Route route =
+      new Route( name, null, parameters, matcher, matchCallback );
+
+    final RouteState state = route.match( location );
+    assertNotNull( state );
+
+    assertEquals( state.getRoute(), route );
+    assertEquals( state.getParameters().size(), 2 );
+    assertEquals( state.getParameters().get( "paramA" ), "ballarat" );
+    assertEquals( state.getParameters().get( "paramB" ), "42" );
+    assertEquals( state.isTerminal(), true );
+  }
+
+  @Test
+  public void match_complexParameters_noMatch()
+  {
+    final RouteMatchCallback matchCallback = new TestRouteMatchCallback();
+    final String name = ValueUtil.randomString();
+    final PathParameter[] parameters = {
+      new PathParameter( "paramA", new TestRegExp( new String[]{ "ballarat" } ) ),
+      new PathParameter( "paramB", new TestRegExp() )
+    };
+    final String location = "/locations/ballarat/events/42";
+    final String[] resultGroups = { location, "ballarat", "42" };
+
+    //Assume a regexp like "^/locations/([^/]+)/events/(\d+)$"
+    final TestRegExp matcher = new TestRegExp( resultGroups );
+    final Route route =
+      new Route( name, null, parameters, matcher, matchCallback );
+
+    final RouteState state = route.match( location );
+    assertNull( state );
+  }
+
+  @Test
   public void getParameterByIndex_withBadIndex()
   {
     final RouteMatchCallback matchCallback = new TestRouteMatchCallback();
