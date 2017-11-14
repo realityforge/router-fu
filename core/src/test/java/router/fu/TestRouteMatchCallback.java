46f2bcdae589dd2786efe403589353e04a887ac6
Peter Donald <peter@realityforge.org>
2017-11-14 11:41:50 +1100

    Make the result configurable

--- core/src/test/java/router/fu/TestRouteMatchCallback.java
+++ core/src/test/java/router/fu/TestRouteMatchCallback.java
@@ -1,17 +1,30 @@
 package router.fu;
 
 import java.util.Map;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 
 class TestRouteMatchCallback
   implements RouteMatchCallback
 {
+  private final MatchResult _result;
+
+  TestRouteMatchCallback()
+  {
+    this( MatchResult.TERMINAL );
+  }
+
+  TestRouteMatchCallback( @Nonnull final MatchResult result )
+  {
+    _result = Objects.requireNonNull( result );
+  }
+
   @Nonnull
   @Override
   public MatchResult shouldMatch( @Nonnull final String location,
                                   @Nonnull final Route route,
                                   @Nonnull final Map<String, String> parameters )
   {
-    return MatchResult.TERMINAL;
+    return _result;
   }
 }
