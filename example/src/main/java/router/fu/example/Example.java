23a9e0352b8c0261f6e3d3582f3ed30e5a78e401
Peter Donald <peter@realityforge.org>
2017-11-15 12:19:57 +1100

    Add no-op entrypoint for compilation purposes

---
+++ example/src/main/java/router/fu/example/Example.java
@@ -0,0 +1,12 @@
+package router.fu.example;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Example
+  implements EntryPoint
+{
+  @Override
+  public void onModuleLoad()
+  {
+  }
+}
