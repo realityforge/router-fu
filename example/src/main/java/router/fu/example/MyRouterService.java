3a78bb4c517861adb2617cf7c3b5b2eb0175559a
Peter Donald <peter@realityforge.org>
2017-11-15 12:21:03 +1100

    Add possible shape of generated router

---
+++ example/src/main/java/router/fu/example/MyRouterService.java
@@ -0,0 +1,46 @@
+package router.fu.example;
+
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import router.fu.RouteLocation;
+
+/**
+ * Possible shape of generated router interface.
+ */
+@Generated( "router.fu" )
+public interface MyRouterService
+{
+  @Nonnull
+  String buildRegionsLocation();
+
+  void gotoRegions();
+
+  @Nonnull
+  String buildRegionLocation( @Nonnull String regionCode );
+
+  void gotoRegion( @Nonnull String regionCode );
+
+  @Nonnull
+  String buildRegionEventsLocation( @Nonnull String regionCode );
+
+  void gotoRegionEvents( @Nonnull String regionCode );
+
+  @Nonnull
+  String buildRegionEventLocation( @Nonnull String regionCode, @Nonnull String eventId );
+
+  void gotoRegionEvent( @Nonnull String regionCode, @Nonnull String eventId );
+
+  @Nullable
+  String getRegionCode();
+
+  void setRegionCode( @Nonnull String regionCode );
+
+  @Nullable
+  String getEventId();
+
+  void setEventId( @Nonnull String EventId );
+
+  @Nonnull
+  RouteLocation getLocation();
+}
