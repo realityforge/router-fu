65b985ffd1e1fdf5066b332f8eeefdaf5eeb8d8e
Peter Donald <peter@realityforge.org>
2017-11-20 13:53:54 +1100

    Update example module so can perform some manual testing

--- example/src/main/java/router/fu/example/Example.java
+++ example/src/main/java/router/fu/example/Example.java
@@ -1,6 +1,9 @@
 package router.fu.example;
 
 import com.google.gwt.core.client.EntryPoint;
+import elemental2.dom.DomGlobal;
+import java.util.Random;
+import org.realityforge.arez.Arez;
 
 public class Example
   implements EntryPoint
@@ -8,5 +11,38 @@ public class Example
   @Override
   public void onModuleLoad()
   {
+    // Need to force AppState clinit to be run to ensure that it is run as top level transaction
+    @SuppressWarnings( "unused" )
+    final MyRouterService router = AppState.ROUTER;
+
+    DomGlobal.document.querySelector( "#login" ).
+      addEventListener( "click", e -> Arez.context().safeAction( () -> AppState.AUTH.setUsername( "Bob" ) ) );
+    DomGlobal.document.querySelector( "#logout" ).
+      addEventListener( "click", e -> Arez.context().safeAction( () -> AppState.AUTH.setUsername( null ) ) );
+    DomGlobal.document.querySelector( "#route_regions" ).
+      addEventListener( "click", e -> AppState.ROUTER.gotoRegions() );
+    DomGlobal.document.querySelector( "#route_region" ).
+      addEventListener( "click", e -> AppState.ROUTER.gotoRegion( "ballarat" ) );
+    DomGlobal.document.querySelector( "#route_regionEvents" ).
+      addEventListener( "click", e -> AppState.ROUTER.gotoRegionEvents( "ballarat" ) );
+    DomGlobal.document.querySelector( "#route_regionEvent" ).
+      addEventListener( "click", e -> AppState.ROUTER.gotoRegionEvent( "ballarat", "42" ) );
+    DomGlobal.document.querySelector( "#randomize_regionCode" ).
+      addEventListener( "click", e -> AppState.ROUTER.updateRegionCode( String.valueOf( new Random().nextInt() ) ) );
+    DomGlobal.document.querySelector( "#randomize_eventId" ).
+      addEventListener( "click", e -> AppState.ROUTER.updateEventId( String.valueOf( new Random().nextInt() ) ) );
+    Arez.context().autorun( this::renderView );
+  }
+
+  private void renderView()
+  {
+
+    final String username = AppState.AUTH.getUsername();
+    DomGlobal.document.querySelector( "#username" ).innerHTML = null == username ? "" : username;
+    DomGlobal.document.querySelector( "#location" ).innerHTML = AppState.ROUTER.getLocation().getPath();
+    final String regionCode = AppState.ROUTER.getRegionCode();
+    DomGlobal.document.querySelector( "#regionCode" ).innerHTML = null == regionCode ? "" : regionCode;
+    final String eventId = AppState.ROUTER.getEventId();
+    DomGlobal.document.querySelector( "#eventId" ).innerHTML = null == eventId ? "" : eventId;
   }
 }
