92e7fca947ae3a060b98c75e5d854f9702846f44
Peter Donald <peter@realityforge.org>
2017-11-20 14:25:30 +1100

    Add javadocs

--- annotations/src/main/java/router/fu/annotations/Router.java
+++ annotations/src/main/java/router/fu/annotations/Router.java
@@ -4,9 +4,17 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks class to be processed by RouterFu annotation processor.
+ */
 @Documented
 @Target( ElementType.TYPE )
 public @interface Router
 {
+  /**
+   * Return true if router should be an Arez component.
+   *
+   * @return true if router should be an Arez component.
+   */
   boolean arez() default false;
 }
