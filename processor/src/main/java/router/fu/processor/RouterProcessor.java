5fa5685c22a730860c6b7af74a6d564798d907fb
Peter Donald <peter@realityforge.org>
2019-12-25 15:32:06 +1100

    Reuse utility

--- processor/src/main/java/router/fu/processor/RouterProcessor.java
+++ processor/src/main/java/router/fu/processor/RouterProcessor.java
@@ -1,6 +1,5 @@
 package router.fu.processor;
 
-import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.TypeSpec;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -29,6 +28,7 @@ import javax.lang.model.type.TypeMirror;
 import org.realityforge.proton.AbstractStandardProcessor;
 import org.realityforge.proton.AnnotationsUtil;
 import org.realityforge.proton.ElementsUtil;
+import org.realityforge.proton.GeneratorUtil;
 import org.realityforge.proton.MemberChecks;
 import org.realityforge.proton.ProcessorException;
 
@@ -84,10 +84,7 @@ public final class RouterProcessor
   private void emitTypeSpec( @Nonnull final String packageName, @Nonnull final TypeSpec typeSpec )
     throws IOException
   {
-    JavaFile.builder( packageName, typeSpec ).
-      skipJavaLangImports( true ).
-      build().
-      writeTo( processingEnv.getFiler() );
+    GeneratorUtil.emitJavaType( packageName, typeSpec, processingEnv.getFiler() );
   }
 
   @Nonnull
