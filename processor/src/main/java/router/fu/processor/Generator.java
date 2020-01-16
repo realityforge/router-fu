389ec35b756ed361ec0ae56781c0dd5312afb65e
Peter Donald <peter@realityforge.org>
2020-01-16 16:10:00 +1100

    Optimize imports

--- processor/src/main/java/router/fu/processor/Generator.java
+++ processor/src/main/java/router/fu/processor/Generator.java
@@ -26,7 +26,6 @@ import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.ExecutableType;
 import org.realityforge.proton.GeneratorUtil;
 
